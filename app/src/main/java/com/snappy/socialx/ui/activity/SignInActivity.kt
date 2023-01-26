4d05587b8314ef10985e7622da5dd2fe1896cdd2
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-26 15:14:13 +0530

    Done

--- app/src/main/java/com/snappy/socialx/ui/activity/SignInActivity.kt
+++ app/src/main/java/com/snappy/socialx/ui/activity/SignInActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -17,7 +20,7 @@ import com.snappy.socialx.ui.fragments.SignUpFragment
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
--- app/src/main/java/com/snappy/socialx/ui/fragments/LogInFragment.kt
+++ app/src/main/java/com/snappy/socialx/ui/fragments/LogInFragment.kt
@@ -8,15 +8,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.snappy.socialx.ui.activity.MainActivity
+import com.snappy.socialx.R
 import com.snappy.socialx.databinding.FragmentLogInBinding
+import com.snappy.socialx.ui.activity.MainActivity
 
 class LogInFragment: Fragment() {
+
     private lateinit var binding: FragmentLogInBinding
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code:Int=123
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,6 +40,20 @@ class LogInFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient= GoogleSignIn.getClient(requireContext(),gso)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+
+        binding.googleSignIn.setOnClickListener{ view: View? ->
+            Toast.makeText(requireContext(),"Logging In",Toast.LENGTH_SHORT).show()
+            signInGoogle()
+        }
+
 
         binding.loginButton.setOnClickListener {
             when {
@@ -70,9 +96,52 @@ class LogInFragment: Fragment() {
 
         }
     }
+
+    private  fun signInGoogle(){
+
+        val signInIntent: Intent =mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,Req_Code)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==Req_Code){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+//            firebaseAuthWithGoogle(account!!)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account: GoogleSignInAccount? =completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException){
+            Toast.makeText(requireContext(),e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun UpdateUI(account: GoogleSignInAccount){
+        val credential= GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task->
+            if(task.isSuccessful) {
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
+            }
+        }
+    }
+
+
     override fun onStart() {
         super.onStart()
         val currentUser = Firebase.auth.currentUser
+        if(GoogleSignIn.getLastSignedInAccount(requireContext())!=null){
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+            activity?.finish()
+        }
         updateUI(currentUser)
     }
     private fun updateUI(firebaseUser: FirebaseUser?) {
